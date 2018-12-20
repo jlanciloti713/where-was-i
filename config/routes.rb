c674fb22bde0360d1af0422388d8155cf36bdb8a
Jack Lanciloti <jlanciloti713@gmail.com>
2018-12-19 21:18:43 -0600

    Added index and show page for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'users#index'
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
 end
